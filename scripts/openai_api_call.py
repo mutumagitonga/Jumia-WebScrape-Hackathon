140d23b3575d678ad96db7645060439fa26eea30
mutumagitonga <mutumamwiti11@gmail.com>
2024-03-06 23:00:28 +0300

    Add GEMINI API invocation code to categorize product using product name as input

--- scripts/openai_api_call.py
+++ scripts/openai_api_call.py
@@ -1,8 +1,12 @@
 import os
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 # Load the .env using the dotenv(python-dotenv) package
 load_dotenv()
+
+# Pass api key to the genai client
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
 
 def predict_product_category(name_of_product):
@@ -27,6 +31,22 @@ def predict_product_category(name_of_product):
     Product Name: "{}"
     """.format(name_of_product)
 
+    # Set up the model
+    generation_config = {
+      "temperature": 0.9,
+      "top_p": 1,
+      "top_k": 1,
+      "max_output_tokens": 2048,
+    }
 
-product_name = "NIVEA Radiant & Beauty Advanced Care Lotion"
+    # Define the model & its parameters
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                  generation_config=generation_config,)
+
+    convo = model.start_chat(history=[])  # Start chat
+    convo.send_message(prompt)  # Send the prompt
+    print(convo.last.text)  # Extract the text
+
+
+product_name = "Vitron HTC4388FS - 43\" Smart  Android Frameles"
 predict_product_category(product_name)
