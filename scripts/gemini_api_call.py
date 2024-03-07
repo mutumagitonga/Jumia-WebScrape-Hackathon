e70044bd383a36bbdbd670254775f2b4186c3c5b
mutumagitonga <mutumamwiti11@gmail.com>
2024-03-07 19:05:35 +0300

    Update gemini_api_call script (shorten it)

--- scripts/gemini_api_call.py
+++ scripts/gemini_api_call.py
@@ -14,46 +14,15 @@ def predict_product_category(name_of_product):
     # Define the prompt with product category constraints (as categorized on jumia.co.ke)
     prompt = """
     Given a product name, predict the product category from the following options:
-    - Computing
-    - Electronics
-    - Sporting Goods
-    - Garden & Outdoors
-    - Musical Instruments
-    - Phones & Tablets
-    - Toys & Games
-    - Fashion
-    - Pet Supplies
-    - Home & Office
-    - Automobile
-    - Health & Beauty
-    - Baby Products
-    - Industrial & Scientific
+    Computing, Electronics, Sporting Goods, Garden & Outdoors, Musical Instruments, Phones & Tablets, 
+    Toys & Games, Fashion, Pet Supplies, Home & Office, 
+    Automobile, Health & Beauty, Baby Products, Industrial & Scientific
 
     Product Name: "{}"
     """.format(name_of_product)
 
-    # # Set up the model
-    # generation_config = {
-    #   "temperature": 0.9,
-    #   "top_p": 1,
-    #   "top_k": 1,
-    #   "max_output_tokens": 2048,
-    # }
-
-    # # Define the model & its parameters
-    # model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-    #                               generation_config=generation_config,)
-
-    # convo = model.start_chat(history=[])  # Start chat
-    # convo.send_message(prompt)  # Send the prompt
-    # # print(convo.last.text)  # Extract the text
-    # time.sleep(3)
-    # return convo.last.text  # Return AI response
-
-    # Define the model & its parameters
-    model = genai.GenerativeModel(model_name="gemini-1.0-pro")
+    model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(prompt)
     return response.text
 
 
-# predict_product_category("AILYONS FK-0301 Stainless Steel 1.8L Electric")
