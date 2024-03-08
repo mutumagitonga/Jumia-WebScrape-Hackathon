64ec0e4e7055e8d80ea501ad45a316c03d005405
mutumagitonga <mutumamwiti11@gmail.com>
2024-03-08 17:37:48 +0300

    Update requirements.txt and the gemini api calling python script

--- scripts/gemini_api_call.py
+++ scripts/gemini_api_call.py
@@ -7,18 +7,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Pass api key to the genai client
-genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY_2"))
 
 
 def predict_product_category(name_of_product):
     # Define the prompt with product category constraints (as categorized on jumia.co.ke)
     prompt = """
-    Given a product name, predict the product category from the following options:
+    I have this product name: "{}" 
+    Predict the product category of the above product name from among the following options:
     Computing, Electronics, Sporting Goods, Garden & Outdoors, Musical Instruments, Phones & Tablets, 
     Toys & Games, Fashion, Pet Supplies, Home & Office, 
     Automobile, Health & Beauty, Baby Products, Industrial & Scientific
 
-    Product Name: "{}"
     """.format(name_of_product)
 
     model = genai.GenerativeModel('gemini-pro')
