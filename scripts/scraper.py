86bf5f8cadc7dac38fbddfca4fffaed26ad9f4f5
mutumagitonga <mutumamwiti11@gmail.com>
2024-03-10 11:00:31 +0300

    Move scraper function & html-beautiful converter to scripts module (scraper.py)

---
+++ scripts/scraper.py
@@ -0,0 +1,25 @@
+import requests as req
+from bs4 import BeautifulSoup
+
+
+# Define function to fetch website html data
+def fetch_html_data(web_address):
+    try:
+        # print(f"\nFetching data from {web_address}...")
+        res = req.get(web_address)
+        return res
+    except req.exceptions.RequestException as e:
+        print('Stopped:', e)
+    except TypeError as e:
+        print('Stopped:', e)
+
+
+# Convert web data into beautiful soup object
+def convert_web_data_to_beautiful_soup_obj(web_data):
+    try:
+        # print("Creating BeautifulSoup object...")
+        soup_obj = BeautifulSoup(web_data.text, "html.parser")
+        # print("Success! Object created!")
+        return soup_obj
+    except Exception as e:
+        print("Stopped:", e)
