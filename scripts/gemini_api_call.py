345554f29949fd35de7582ea731eecab0c61b12e
mutumagitonga <mutumamwiti11@gmail.com>
2024-03-13 18:12:13 +0300

    Update the gemini api call script with safety settings minimum level

--- scripts/gemini_api_call.py
+++ scripts/gemini_api_call.py
@@ -1,6 +1,7 @@
 import os
 import time
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from dotenv import load_dotenv
 
 # Load the .env using the dotenv(python-dotenv) package
@@ -11,22 +12,25 @@ genai.configure(api_key=os.environ.get("GEMINI_API_KEY_2"))
 
 
 def predict_product_category(name_of_product):
-    try:
-        # Define the prompt with product category constraints (as categorized on jumia.co.ke)
-        prompt = """
-            I have this product name: "{}" 
-            Predict the product category of the above product name from among the following options:
-            Computing, Electronics, Sporting Goods, Garden & Outdoors, Musical Instruments, Phones & Tablets, 
-            Toys & Games, Fashion, Pet Supplies, Home & Office, 
-            Automobile, Health & Beauty, Baby Products, Industrial & Scientific
+    # try:
+    # Define the prompt with product category constraints (as categorized on jumia.co.ke)
+    prompt = """
+        I have this product name: "{}" 
+        Predict the product category of the above product name from among the following options:
+        Computing, Electronics, Sporting Goods, Garden & Outdoors, Musical Instruments, Phones & Tablets, 
+        Toys & Games, Fashion, Pet Supplies, Home & Office, 
+        Automobile, Health & Beauty, Baby Products, Industrial & Scientific
 
-            """.format(name_of_product)
+        """.format(name_of_product)
 
-        model = genai.GenerativeModel('gemini-pro')
-        response = model.generate_content(prompt)
-        return response.text
-    except:
-        print(f"Error making API call!")
+    # More: https://ai.google.dev/docs/safety_setting_gemini
+    # Define safety settings
+    safety_settings = {
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE
+    }
 
-
-
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt, safety_settings=safety_settings)
+    return response.text
+    # except:
+    #     print(f"Error making API call!")
