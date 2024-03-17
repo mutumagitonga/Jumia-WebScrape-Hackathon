1bbef22bf37720ae747e72144705b0190816c939
mutumagitonga <mutumamwiti11@gmail.com>
2024-03-17 09:15:12 +0300

    Update script

--- scripts/most_satisfying_product.py
+++ scripts/most_satisfying_product.py
@@ -1,3 +1,10 @@
+def check_percentile
+
+
+def get_rating_at_max_votes_value(index):
+    return index
+
+
 def get_max_votes_value_idx(votes):
     target_val = max(votes)  # Get the max value
 
@@ -6,6 +13,7 @@ def get_max_votes_value_idx(votes):
     # If just 1 max value exists, get its index
     if len(target_val_indices) == 1:
         idx = target_val_indices[0]
-        return idx
+        get_rating_at_max_votes_value(idx)
 
-def get_most_satisfying_product():
+
+def get_most_satisfying_product(df):
